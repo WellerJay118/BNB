a47c11debeba1a994a3c0a189034c7e300d07ee4
aAJacob <wellerjay118@gmail.com>
2021-08-19 13:06:13 -0500

    fixed advert issue

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -4,31 +4,31 @@ const SplashPage = () => {
     //things and stuff
 
     return (
-        <div className="splash-wrapper">
-        {/* <div className="search-banner"> */}
-            <div className="searchbar">
-                <input className="search" type="text" placeholder="Search"></input>
-                <input className="guests" type="number" placeholder="Guest Count"></input>
-                <label>Check-in </label>
-                <input className="start-dates" type="date"></input>
-                <label>Check-out </label>
-                <input className="end-dates" type="date"></input>
+            <div className="splash__wrapper">
+                <div className="splash__searchbar-container">
+                    <div className="splash__searchbar">
+                        <input className="search" type="text" placeholder="Search"></input>
+                        <input className="guests" type="number" placeholder="Guest Count"></input>
+                        <label>Check-in </label>
+                        <input className="start-dates" type="date"></input>
+                        <label>Check-out </label>
+                        <input className="end-dates" type="date"></input>
+                    </div>
+                </div>
+                <div className="splash__tisements-container">
+                    <div className="splash__tisements-container--toprated">
+                        <p>Top Rated spot</p>
+                    </div>
+                    <div className="splash__tisements-container--signup" >
+                        <NavLink style={{ textDecoration: 'none', color: 'black'}}to="/signup">
+                        <p>Join Us!</p>
+                        </NavLink>
+                    </div>
+                    <div className="splash__tisements-container--lastbooked">
+                        <p>Last booked!</p>
+                    </div>
+                </div>
             </div>
-        {/* </div> */}
-        {/* <div className="adverts"> */}
-            <div className="top-rated">
-                <p>Top Rated spot</p>
-            </div>
-            <div className="extra-signup" >
-                <NavLink style={{ textDecoration: 'none', color: 'black'}}to="/signup">
-                <p>Join Us!</p>
-                </NavLink>
-            </div>
-            <div className="last-booked">
-                <p>Last booked!</p>
-            </div>
-        {/* </div> */}
-        </div>
     )
 }
 export default SplashPage;
