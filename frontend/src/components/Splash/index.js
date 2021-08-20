6f15bbe01348312aae88782649769548ba93fd21
aAJacob <wellerjay118@gmail.com>
2021-08-20 11:54:19 -0500

    noticed bug of fonts being different on some links

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -5,16 +5,18 @@ const SplashPage = () => {
 
     return (
             <div className="splash__wrapper">
-                <div className="splash__searchbar-container">
-                    <input className="splash__searchbar" type="text" placeholder="Search"></input>
-                    <input className="splash__guests" type="number" placeholder="Guest Count"></input>
-                    <label>Check-in</label>
-                    <input className="splash__startdates" type="date"></input>
-                    <label>Check-out</label>
-                    <input className="splash__enddates" type="date"></input>
+                <div className="splash__searchbar-wrapper">
+                    <div className="splash__searchbar-container">
+                        <input className="splash__searchbar" type="text" placeholder="Search"></input>
+                        <input className="splash__guests" type="number" placeholder="Guest Count"></input>
+                        <label>Check-in</label>
+                        <input className="splash__startdates" type="date"></input>
+                        <label>Check-out</label>
+                        <input className="splash__enddates" type="date"></input>
+                    </div>
                 </div>
 
-                <div className="splash__tisements-container">
+                {/* <div className="splash__tisements-container">
                     <div className="splash__tisements-container--toprated">
                         <p>Top Rated spot</p>
                     </div>
@@ -26,7 +28,7 @@ const SplashPage = () => {
                     <div className="splash__tisements-container--lastbooked">
                         <p>Last booked!</p>
                     </div>
-                </div>
+                </div> */}
             </div>
     )
 }
