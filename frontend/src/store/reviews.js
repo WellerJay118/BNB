a6e59780c50c9a340ddeb794a0cc079b0059e56f
aAJacob <wellerjay118@gmail.com>
2021-08-20 14:21:31 -0500

    reviews store backend route created

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -11,7 +11,7 @@ const loadR = (reviews) = ({
     reviews,
 });
 
-const addR = (reviews) => ({
+const addR = (review) => ({
     type: ADD_REVIEW,
     review
 });
@@ -24,7 +24,57 @@ const removeR = (id) => ({
 
 //define thunk creator
 export const getReview = () => async(dispatch) => {
-    const res = await fetch('/api/')
+    const res = await fetch('/api/reviews');
+    const reviews = await res.json();
+    dispatch(loadR(reviews));
 }
 
+export const submitReview = (review) => async(dispatch) => {
+    const res = await csrfFetch('/api/reviews', {
+        method: "POST",
+        body: JSON.stringify(review)
+    })
+    if(res.ok) {
+        const newReview = await res.json();
+        dispatch(addR(newReview));
+        return newReview;
+    }
+};
+
+export const deleteReview = (id) => async(dispatch) => {
+    const res = await csrfFetch(`/api/reviews/${id}`, {
+        method: "DELETE",
+    })
+    if (res.ok) {
+        dispatch(removeR(id));
+    }
+};
+
+//define intial state
+const initialState = {};
+
+//define reducer
+const reviewsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case LOAD_REVIEW: {
+            const newState = {...state}
+            action.reviews.forEacht(review => {
+                newState[review.id] = review
+            })
+            return newState;
+        }
+        case ADD_REVIEW: {
+            const newState = {...state}
+            newState[action.review.id] = action.review
+            return newState
+        }
+        case REMOVE_REVIEW: {
+            const newState = {...state}
+            delete newState[action.id]
+            return newState
+        }
+        default:
+            return state;
+    }
+}
 export default reviewsReducer;
